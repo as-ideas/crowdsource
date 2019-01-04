c0f9cf3844db98d85bf641b9f36419ad86f7009d
Madany Mamlouk, Tarek <tarek.madanymamlouk@asideas.de>
2019-01-04 12:12:31 +0100

    add toggling to checkbox

--- crowdsource-frontend/src/views/RegisterView.js
+++ crowdsource-frontend/src/views/RegisterView.js
@@ -9,43 +9,55 @@ import PrimaryButton from "../atoms/buttons/PrimaryButton";
 
 class RegisterView extends React.Component {
 
-    state = {
-        email: "",
-        acceptAGB: false
-    }
+  state = {
+    email: "",
+    acceptAGB: false
+  }
 
-    render() {
-        return (
-            <div>
-                <Header />
-                <AuthConsumer>
-                    {({ isAuth }) => (
-                        <FormBox>
-                            {isAuth ? (
-                                <div>
-                                    {/* If used is logged in, redirect to start page */}
-                                    <Redirect to="/"/>
-                                </div>
-                            ) : (
-                                <div>
-                                    <h1>Registrierung</h1>
-                                    <Form>
-                                        <label htmlFor="email">Email-Adresse</label>
-                                        <input type="text" id="email" placeholder="max.mustermann" autoComplete="username" value="crowdsource@crowd.source.de" onChange={event => this.setState({ email: event.target.value })} />
-                                        <input id="agb" type="checkbox" onChange={ event => this.setState({acceptAGB: event.target.value}) }/>
-                                        <label htmlFor="agb">Ich akzeptiere die <Link to="#">AGB</Link></label>
-                                        <PrimaryButton label="Registrieren" onClick={ () => alert('Not implemented yet')}/>
-                                    </Form>
-                                    <Link to="login">oder hier anmelden</Link>
-                                </div>
-                            )}
+  toggleAGBBox = () => {
+    this.setState({acceptAGB: !this.state.acceptAGB})
+  }
 
-                        </FormBox>
-                    )}
-                </AuthConsumer>
-            </div>
-        )
-    }
+  signup = (email, acceptAGB) => {
+    alert("email: " + email + ", acceptAGB: " + acceptAGB)
+  }
+
+  render() {
+    return (
+        <div>
+          <Header/>
+          <AuthConsumer>
+            {({isAuth}) => (
+                <FormBox>
+                  {isAuth ? (
+                      <div>
+                        {/* If used is logged in, redirect to start page */}
+                        <Redirect to="/"/>
+                      </div>
+                  ) : (
+                      <div>
+                        <h1>Registrierung</h1>
+                        <Form>
+                          <label htmlFor="email">Email-Adresse</label>
+                          <input type="text" id="email" placeholder="max.mustermann" autoComplete="username"
+                                 value="crowdsource@crowd.source.de"
+                                 onChange={event => this.setState({email: event.target.value})}/>
+                          <input id="agb" type="checkbox" checked={!!this.state.acceptAGB}
+                                 onChange={this.toggleAGBBox}/>
+                          <label htmlFor="agb">Ich akzeptiere die <Link to="#">AGB</Link></label>
+                          <PrimaryButton label="Registrieren"
+                                         onClick={() => this.signup(this.state.email, this.state.acceptAGB)}/>
+                        </Form>
+                        <Link to="login">oder hier anmelden</Link>
+                      </div>
+                  )}
+
+                </FormBox>
+            )}
+          </AuthConsumer>
+        </div>
+    )
+  }
 }
 
 export default RegisterView
