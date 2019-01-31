8b1a238cbc57c73c222cef9c1fc98c39d7380b9e
Julian Godesa <julian.godesa@asideas.de>
2019-01-31 14:31:04 +0100

    no-ticket - fixes status bar active state; adds video url to teaser

--- crowdsource-frontend/src/main/resources/public/app/ideas/ideas-status-bar/ideas-status-bar.js
+++ crowdsource-frontend/src/main/resources/public/app/ideas/ideas-status-bar/ideas-status-bar.js
@@ -3,7 +3,7 @@ angular.module('crowdsource')
         return {
             restrict: 'E',
             scope: {
-                'campaign':'='
+                'campaign': '='
             },
             controllerAs: 'status',
             bindToController: true,
@@ -12,11 +12,10 @@ angular.module('crowdsource')
                 var vm = this;
                 vm.breadcrumbs = [];
                 vm.userIsAdmin = Authentication.isAdmin();
-                vm.campaignRunning = campaignIsRunning();
 
-              if (!vm.campaign) {
-                throw Error('ideas-status-bar: missing campaign on scope in directive');
-              }
+                if (!vm.campaign) {
+                    throw Error('ideas-status-bar: missing campaign on scope in directive');
+                }
 
                 function updateTitle(title) {
                     var newTitle = "CrowdSource";
@@ -29,22 +28,16 @@ angular.module('crowdsource')
 
                 function updateBreadcrumb(title) {
                     vm.breadcrumbs = [];
-                    vm.breadcrumbs.push({target:'/#/intro', label:'CROWD'});
-                    vm.breadcrumbs.push({target:'/#/ideas', label:title});
-                    vm.breadcrumbs.push({target:'/#/ideas/' + vm.campaign.id, label:vm.campaign.title});
-                }
-
-                function campaignIsRunning() {
-                    var nowMs = new Date().getDate();
-                  var endDateMs = (vm.campaign && vm.campaign.endDate) ? vm.campaign.endDate : 0;
-                    return nowMs > endDateMs;
+                    vm.breadcrumbs.push({target: '/#/intro', label: 'CROWD'});
+                    vm.breadcrumbs.push({target: '/#/ideas', label: title});
+                    vm.breadcrumbs.push({target: '/#/ideas/' + vm.campaign.id, label: vm.campaign.title});
                 }
 
                 Route.onRouteChangeSuccessAndInit(function (event, currentRoute) {
-                  if (typeof (currentRoute) !== 'undefined' && currentRoute.title) {
-                    updateBreadcrumb(currentRoute.title);
-                    updateTitle(currentRoute.title);
-                  }
+                    if (typeof (currentRoute) !== 'undefined' && currentRoute.title) {
+                        updateBreadcrumb(currentRoute.title);
+                        updateTitle(currentRoute.title);
+                    }
                 });
             }
         };
---
+++ crowdsource-frontend/src/test/javascript/app/util/trust-url-filter.test.js
@@ -0,0 +1,38 @@
+describe('trust-url filter', function () {
+
+    var $compile, $rootScope, $scope;
+
+    beforeEach(function () {
+        module('crowdsource');
+        module('crowdsource.templates');
+
+        inject(function (_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $rootScope = _$rootScope_;
+            $scope = $rootScope.$new();
+        });
+    });
+    describe('trustUrl', function () {
+        it("should render truested url", function () {
+            $scope.videoUrl = "https://www.google.de";
+            var view = $compile('<div><video src="{{videoUrl | trustUrl}}" controls></video></div>')($scope);
+            $scope.$digest();
+
+            console.log(view.html());
+            expect(view.html()).toContain($scope.videoUrl);
+        });
+
+        it("should not render truested url when filter is missing", function () {
+            $scope.videoUrl = "https://www.google.de";
+            var view;
+            try {
+                // will throw interpolation error
+                view = $compile('</div><video src="{{videoUrl}}" controls></video></div>')($scope);
+                $scope.$digest();
+            } catch (e) {
+            }
+
+            expect(view).not.toBeTruthy();
+        });
+    });
+});
