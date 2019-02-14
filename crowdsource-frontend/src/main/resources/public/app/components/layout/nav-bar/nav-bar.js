d0b4dc4f927b149d88b77c6a99959275ccc958ac
Lars Reith <lars.reith@asideas.de>
2019-02-14 13:22:40 +0100

    CROWD-364 FIX initial state on mobile

--- crowdsource-frontend/src/main/resources/public/app/components/layout/nav-bar/nav-bar.js
+++ crowdsource-frontend/src/main/resources/public/app/components/layout/nav-bar/nav-bar.js
@@ -3,124 +3,131 @@ angular.module('crowdsource')
     .directive('navBar', function () {
         var directive = {};
 
+        directive.templateUrl = 'app/components/layout/nav-bar/nav-bar.html';
         directive.controllerAs = 'vm';
         directive.bindToController = true;
-        directive.templateUrl = 'app/components/layout/nav-bar/nav-bar.html';
         directive.controller = ['$scope','$window','Authentication','Route','Idea','ROUTE_DETAILS', NavBarController]
+
+        function NavBarController($scope, $window, Authentication, Route, Idea, ROUTE_DETAILS) {
+            var vm = this;
+
+            vm.auth = Authentication;
+            vm.breadcrumbs = [];
+            vm.localNavItems = [];
+            vm.isMobile = isMobileSize();
+            vm.isMobileMenuOpen = false;
+
+            console.log("isMobile: " + vm.isMobile);
+            console.log("isMobileMenuOpen" + vm.isMobileMenuOpen);
+
+            vm.toggleMobileMenu = toggleMobileMenu;
+            vm.closeMobileMenu = closeMobileMenu;0
+
+            // Watch for changes in the idea service
+            $scope.$watch(function(){
+                return Idea.currentCampaign;
+            }, ideaServiceObserver)
+
+            // Window resize listener for mobile menu
+            $window.addEventListener('resize', function() {
+                var oldValue = vm.isMobile;
+                var newValue = isMobileSize();
+
+                if(oldValue != newValue) {
+                    vm.isMobile = newValue;
+                    $scope.$digest();
+                }
+            })
+
+            function isMobileSize() {
+                var isM = $window.innerWidth <= 768 ? true : false;
+                console.log("isMobileSize: " + isM);
+                return isM;
+            };
+
+            function toggleMobileMenu() {
+                console.log("toggle")
+                vm.isMobileMenuOpen = !vm.isMobileMenuOpen;
+            }
+
+            function closeMobileMenu() {
+                console.log("close")
+                vm.isMobileMenuOpen = false;
+            }
+
+            function updateWindowTitle(breadcrumb) {
+
+                var prefix = "CrowdSource - ";
+                var breadcrumbTitles = [];
+
+                for (var i=0; i<breadcrumb.length; i++) {
+                    breadcrumbTitles.push(breadcrumb[i].label);
+                }
+
+                var title = prefix + breadcrumbTitles.join(" - ");
+                $window.document.title = title;
+            }
+
+            function updateNavigationAndWindowTitle(currentRoute) {
+                vm.breadcrumbs = [];
+                vm.localNavItems = [];
+
+                if(isIdeasCampaign(currentRoute)) {
+                    if (Idea.currentCampaign) {
+                        vm.breadcrumbs = getIdeasBreadcrumb(currentRoute);
+                        vm.localNavItems = getIdeasLocalNavItems();
+
+                    }
+                }
+
+                else {
+                    vm.breadcrumbs.push({target: '#', label: currentRoute.title});
+                }
+
+                updateWindowTitle(vm.breadcrumbs);
+            }
+
+            function getIdeasBreadcrumb(currentRoute) {
+                var breadcrumbs = [];
+                breadcrumbs.push({target: '/#/ideas/' + Idea.currentCampaign.id, label: Idea.currentCampaign.title});
+
+                if (currentRoute[ROUTE_DETAILS.JSON_ROOT]
+                    && currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_IS_OVERVIEW]
+                    && currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_IS_OVERVIEW] == true)
+                    return breadcrumbs;
+
+                breadcrumbs.push({target: '#', label: currentRoute.title});
+                return breadcrumbs;
+            }
+
+            function getIdeasLocalNavItems() {
+                var localNavItems = [];
+                localNavItems.push({target: '/#/ideas/' + Idea.currentCampaign.id + '/own', label: 'Deine Ideen'});
+                if(Authentication.isAdmin()) localNavItems.push({target: '/#/ideas/' + Idea.currentCampaign.id + '/admin', label: 'Admin'});
+                return localNavItems;
+            }
+
+            function isIdeasCampaign(currentRoute){
+                if (!currentRoute[ROUTE_DETAILS.JSON_ROOT]) return false;
+                if (!currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_CAMPAIGN]) return false;
+                if (!currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_CAMPAIGN] == ROUTE_DETAILS.VALUE_CAMPAIGN_IDEA) return false;
+                return true;
+            }
+
+            function ideaServiceObserver() {
+                if(vm.currentRoute) {
+                    updateNavigationAndWindowTitle(vm.currentRoute);
+                }
+            }
+
+            Route.onRouteChangeSuccessAndInit(function (event, currentRoute) {
+                if (typeof (currentRoute) !== 'undefined' && currentRoute.title) {
+
+                    vm.currentRoute = currentRoute;
+                    updateNavigationAndWindowTitle(currentRoute);
+                }
+            });
+        };
 
         return directive;
     });
-
-
-function NavBarController($scope, $window, Authentication, Route, Idea, ROUTE_DETAILS) {
-    var vm = this;
-
-    vm.auth = Authentication;
-    vm.breadcrumbs = [];
-    vm.localNavItems = [];
-    vm.isMobile; isMobileSize();
-    vm.isMobileMenuOpen = false;
-    vm.toggleMobileMenu = toggleMobileMenu;
-    vm.closeMobileMenu = closeMobileMenu;
-
-    // Watch for changes in the idea service
-    $scope.$watch(function(){
-        return Idea.currentCampaign;
-    }, ideaServiceObserver)
-
-    // Window resize listener for mobile menu
-              $window.addEventListener('resize', function() {
-                  var oldValue = vm.isMobile;
-                  var newValue = isMobileSize();
-
-                  if(oldValue != newValue) {
-                      vm.isMobile = newValue;
-                      $scope.$digest();
-                  }
-              })
-
-    function isMobileSize() {
-        return $window.innerWidth <= 768 ? true : false;
-    };
-
-    function toggleMobileMenu() {
-        vm.isMobileMenuOpen = !vm.isMobileMenuOpen;
-    }
-
-    function closeMobileMenu() {
-        vm.isMobileMenuOpen = false;
-    }
-
-    function updateWindowTitle(breadcrumb) {
-
-        var prefix = "CrowdSource - ";
-        var breadcrumbTitles = [];
-
-        for (var i=0; i<breadcrumb.length; i++) {
-            breadcrumbTitles.push(breadcrumb[i].label);
-        }
-
-        var title = prefix + breadcrumbTitles.join(" - ");
-        $window.document.title = title;
-    }
-
-    function updateNavigationAndWindowTitle(currentRoute) {
-        vm.breadcrumbs = [];
-        vm.localNavItems = [];
-
-        if(isIdeasCampaign(currentRoute)) {
-            if (Idea.currentCampaign) {
-                vm.breadcrumbs = getIdeasBreadcrumb(currentRoute);
-                vm.localNavItems = getIdeasLocalNavItems();
-
-            }
-        }
-
-        else {
-            vm.breadcrumbs.push({target: '#', label: currentRoute.title});
-        }
-
-        updateWindowTitle(vm.breadcrumbs);
-    }
-
-    function getIdeasBreadcrumb(currentRoute) {
-        var breadcrumbs = [];
-        breadcrumbs.push({target: '/#/ideas/' + Idea.currentCampaign.id, label: Idea.currentCampaign.title});
-
-        if (currentRoute[ROUTE_DETAILS.JSON_ROOT]
-            && currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_IS_OVERVIEW]
-            && currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_IS_OVERVIEW] == true)
-            return breadcrumbs;
-
-        breadcrumbs.push({target: '#', label: currentRoute.title});
-        return breadcrumbs;
-    }
-
-    function getIdeasLocalNavItems() {
-        var localNavItems = [];
-        localNavItems.push({target: '/#/ideas/' + Idea.currentCampaign.id + '/own', label: 'Deine Ideen'});
-        if(Authentication.isAdmin()) localNavItems.push({target: '/#/ideas/' + Idea.currentCampaign.id + '/admin', label: 'Admin'});
-        return localNavItems;
-    }
-
-    function isIdeasCampaign(currentRoute){
-        if (!currentRoute[ROUTE_DETAILS.JSON_ROOT]) return false;
-        if (!currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_CAMPAIGN]) return false;
-        if (!currentRoute[ROUTE_DETAILS.JSON_ROOT][ROUTE_DETAILS.ATTR_CAMPAIGN] == ROUTE_DETAILS.VALUE_CAMPAIGN_IDEA) return false;
-        return true;
-    }
-
-    function ideaServiceObserver() {
-        if(vm.currentRoute) {
-            updateNavigationAndWindowTitle(vm.currentRoute);
-        }
-    }
-
-    Route.onRouteChangeSuccessAndInit(function (event, currentRoute) {
-        if (typeof (currentRoute) !== 'undefined' && currentRoute.title) {
-
-            vm.currentRoute = currentRoute;
-            updateNavigationAndWindowTitle(currentRoute);
-        }
-    });
-};
