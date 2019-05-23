8a5e251532ca9aca9b51a0456328e2d2535cfe06
Lars Reith <lars.reith@asideas.de>
2019-05-23 10:56:20 +0200

    CROWD-419 ADD Finalized -pot generation from angular source code

--- crowdsource-frontend/translation/extract-translation.js
+++ crowdsource-frontend/translation/extract-translation.js
@@ -2,15 +2,16 @@ var angularTranslate = require("angular-translate-extract");
 
 
 var data = {
-    src: "src/main/resources/public/app//**/*.html",
+    src: "src/main/resources/public/app/**/*.html",
     dest: "translation",
     prefix: "crowdsource",
     adapter: "pot",
     lang: ["de"]
 }
 
-var basePath = __dirname;
+var basePath = __dirname.substring(0,__dirname.lastIndexOf('/'));
 console.log("basepath: " + basePath);
+console.log("__dirname: " + __dirname);
 
 var option = {
     basePath: basePath
