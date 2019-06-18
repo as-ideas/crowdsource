1072797803378ecb8b8e3b4a2deda5619cf76f38
Lars Reith <lars.reith@asideas.de>
2019-06-18 14:02:30 +0200

    MOD Refactor static pages and translation texts from crowdsource-as into crowdsource

--- crowdsource-frontend/translation/translation-convert.js
+++ crowdsource-frontend/translation/translation-convert.js
@@ -19,21 +19,43 @@ let convert = function() {
     files.forEach(function(file) {
         try {
             let jsonData = po2json.parseFileSync(`${baseDir}/${file}.po`, options);
+            let json = JSON.parse(jsonData);
+            unescapeChars(json);
 
-            jsonData = unescapeChars(jsonData);
-            console.log(jsonData);
-            fs.writeFileSync(`${destDir}/${file}.json`, jsonData);
+            console.log(json);
+            fs.writeFileSync(`${destDir}/${file}.json`, JSON.stringify(json));
         } catch (e) {
             console.error(e);
         }
     })
 }
 
-let unescapeChars = function(text) {
-    let newtext;
-    newtext = text.replace(/\\\\{\\\\{/g,"{{");
-    newtext = newtext.replace(/\\\\}\\\\}/g,"}}");
-    return newtext;
+let unescapeChars = function(json) {
+
+    for (var key in json) {
+        if (json.hasOwnProperty(key)) {
+
+            let text = json[key];
+            text = text.replace(/\\\\{\\\\{/g,"{{").replace(/\\\\}\\\\}/g,"}}");
+
+            // Replace within <p> tags all newlines with </br>
+            // This is for paragraphs
+            // Use cases: Simple formatting of newlines within paragraphs
+            text = text.replace(new RegExp("<p>[^<]*<\/p>", "g"), function (match){
+                return match.replace(new RegExp("\\n","g"),"</br>");
+            })
+
+            // If there are not HTML tags, replace newlines with </br>
+            if(text.indexOf("<") === -1 && text.indexOf(">") === -1 && text.indexOf("\n") > -1) {
+                console.log(key + ": " + json[key]);
+                text = text.replace(new RegExp("\\n","g"),"</br>");
+                console.log(key + ": " + text);
+            }
+
+            json[key] = text;
+        }
+    }
+
 }
 
 convert();
