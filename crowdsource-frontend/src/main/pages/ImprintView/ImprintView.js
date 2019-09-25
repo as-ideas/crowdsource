7b4f61268a79e3ae28155b31f0021e2c0c68d1fe
Madany Mamlouk, Tarek <tarek.madanymamlouk@asideas.de>
2019-09-25 15:00:31 +0200

    fix styling

--- crowdsource-frontend/src/main/pages/ImprintView/ImprintView.js
+++ crowdsource-frontend/src/main/pages/ImprintView/ImprintView.js
@@ -5,42 +5,27 @@ export default class ImprintView extends React.Component {
 
   render () {
     return (
-        <div className='imprintView__container'>
-          <div className='imprintView__content'>
-            <content-row class="imprint">
-              <h2 translate="AS_IMPRINT_HEADLINE">Impressum</h2>
-              <h4 translate="AS_IMPRINT_PROVIDER">Anbieter</h4>
-              <address translate="AS_IMPRINT_ADDRESS">
-                Axel Springer Ideas Engineering GmbH<br/>
-                Ein Unternehmen der Axel Springer SE<br/>
-                Axel-Springer-Straße 65<br/>
-                10888 Berlin
-              </address>
+      <div class="content ng-scope" data-ng-view="" autoscroll="true">
+        <div class="teaser--slim ng-scope"></div>
+        <content-row class="imprint ng-scope">
+          <div class="container" ng-transclude="">
+            <h2 translate="AS_IMPRINT_HEADLINE" class="ng-scope">Imprint</h2>
 
-              <h4 translate="AS_IMPRINT_CONTACT_HEADLINE">Kontakt</h4>
-              <p translate="AS_IMPRINT_CONTACT_P" translate-value-email="<a href='mailto:crowd@asideas.de'>crowd@asideas.de</a>">
-                E-Mail: <a href="mailto:crowd@asideas.de">crowd@asideas.de</a><br/>
-                Telefon: 030 - 2591 78100<br/>
-              </p>
+            <h4 translate="AS_IMPRINT_PROVIDER" class="ng-scope">Provider</h4>
+            <address translate="AS_IMPRINT_ADDRESS" class="ng-scope">Axel Springer Ideas Engineering GmbH<br/>A company of Axel Springer SE<br/>Axel-Springer-Strasse 65<br/>10888 Berlin</address>
 
-              <h4 translate="AS_IMPRINT_RESPONSIBLE_HEADLINE">Verantwortlich für den Inhalt nach § 6 Abs.2 MDStV</h4>
-              <p translate="AS_IMPRINT_RESPONSIBLE_P">
-                  Michael Alber<br/>
-              COO<br/>
-              Axel-Springer-Straße 65<br/>
-              10888 Berlin<br/>
-              <br/>
-              Amtsgericht/ Handelsregister<br/>
-              Sitz Berlin, Amtsgericht Charlottenburg, HRB 138466 B<br/>
-              USt-IdNr. DE 287499537<br/>
-              Geschäftsführer: Samir Fadlallah, Michael Alber<br/>
-              </p>
-              <br/>
-              <h4 translate="AS_IMPRINT_COPYRIGHTS_HEADLINE">Bildrechte</h4>
-              <p translate="AS_IMPRINT_COPYRIGHTS_P"></p>
-            </content-row>
+            <h4 translate="AS_IMPRINT_CONTACT_HEADLINE" class="ng-scope">Contact Information</h4>
+            <p translate="AS_IMPRINT_CONTACT_P" translate-value-email="<a href='mailto:crowd@asideas.de'>crowd@asideas.de</a>" class="ng-scope">E-mail: <a href="mailto:crowd@asideas.de">crowd@asideas.de</a><br/>Phone: 030 - 2591 78100</p>
+
+            <h4 translate="AS_IMPRINT_RESPONSIBLE_HEADLINE" class="ng-scope">Responsible for the content according to § 6 Abs.2 MDStV</h4>
+            <p translate="AS_IMPRINT_RESPONSIBLE_P" class="ng-scope">Michael Alber<br/>COO<br/>Axel-Springer-Strasse 65<br/>10888 Berlin<br/><br/>District Court/Commercial Register<br/>Based in Berlin, District Court of Charlottenburg, HRB 138466 B<br/>VAT ID No. DE 287499537<br/>Executive Directors: Samir Fadlallah, Michael Alber</p>
+            <br class="ng-scope" />
+            <h4 translate="AS_IMPRINT_COPYRIGHTS_HEADLINE" class="ng-scope">&nbsp;</h4>
+            <p translate="AS_IMPRINT_COPYRIGHTS_P" class="ng-scope">&nbsp;</p>
+
           </div>
-        </div>
+        </content-row>
+      </div>
     )
   }
 
