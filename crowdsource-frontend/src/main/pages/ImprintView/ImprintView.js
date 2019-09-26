3431c517a48ce542e2155f07130ff00af3242124
Madany Mamlouk, Tarek <tarek.madanymamlouk@asideas.de>
2019-09-26 12:54:06 +0200

    use new translate component

--- crowdsource-frontend/src/main/pages/ImprintView/ImprintView.js
+++ crowdsource-frontend/src/main/pages/ImprintView/ImprintView.js
@@ -1,27 +1,28 @@
 import React from 'react'
 import './ImprintView.scss'
+import { Trans } from '@lingui/macro';
 
 export default class ImprintView extends React.Component {
 
   render () {
     return (
-      <div class="content ng-scope" data-ng-view="" autoscroll="true">
-        <div class="teaser--slim ng-scope"></div>
-        <content-row class="imprint ng-scope">
-          <div class="container" ng-transclude="">
-            <h2 translate="AS_IMPRINT_HEADLINE" class="ng-scope">Imprint</h2>
+      <div className="content ng-scope" data-ng-view="" autoscroll="true">
+        <div className="teaser--slim ng-scope"></div>
+        <content-row className="imprint ng-scope">
+          <div className="container" ng-transclude="">
+            <h2 className="ng-scope"><Trans id="AS_IMPRINT_HEADLINE" /></h2>
 
-            <h4 translate="AS_IMPRINT_PROVIDER" class="ng-scope">Provider</h4>
-            <address translate="AS_IMPRINT_ADDRESS" class="ng-scope">Axel Springer Ideas Engineering GmbH<br/>A company of Axel Springer SE<br/>Axel-Springer-Strasse 65<br/>10888 Berlin</address>
+            <h4 className="ng-scope"><Trans id="AS_IMPRINT_PROVIDER" /></h4>
+            <address className="ng-scope"><Trans id="AS_IMPRINT_ADDRESS" /></address>
 
-            <h4 translate="AS_IMPRINT_CONTACT_HEADLINE" class="ng-scope">Contact Information</h4>
-            <p translate="AS_IMPRINT_CONTACT_P" translate-value-email="<a href='mailto:crowd@asideas.de'>crowd@asideas.de</a>" class="ng-scope">E-mail: <a href="mailto:crowd@asideas.de">crowd@asideas.de</a><br/>Phone: 030 - 2591 78100</p>
+            <h4 className="ng-scope"><Trans id="AS_IMPRINT_CONTACT_HEADLINE" /></h4>
+            <p translate-value-email="<a href='mailto:crowd@asideas.de'>crowd@asideas.de</a>" className="ng-scope"><Trans id="AS_IMPRINT_CONTACT_P" /></p>
 
-            <h4 translate="AS_IMPRINT_RESPONSIBLE_HEADLINE" class="ng-scope">Responsible for the content according to § 6 Abs.2 MDStV</h4>
-            <p translate="AS_IMPRINT_RESPONSIBLE_P" class="ng-scope">Michael Alber<br/>COO<br/>Axel-Springer-Strasse 65<br/>10888 Berlin<br/><br/>District Court/Commercial Register<br/>Based in Berlin, District Court of Charlottenburg, HRB 138466 B<br/>VAT ID No. DE 287499537<br/>Executive Directors: Samir Fadlallah, Michael Alber</p>
-            <br class="ng-scope" />
-            <h4 translate="AS_IMPRINT_COPYRIGHTS_HEADLINE" class="ng-scope">&nbsp;</h4>
-            <p translate="AS_IMPRINT_COPYRIGHTS_P" class="ng-scope">&nbsp;</p>
+            <h4 className="ng-scope"><Trans id="AS_IMPRINT_RESPONSIBLE_HEADLINE" /></h4>
+            <p className="ng-scope"><Trans id="AS_IMPRINT_RESPONSIBLE_P" /></p>
+            <br className="ng-scope" />
+            <h4 className="ng-scope"><Trans id="AS_IMPRINT_COPYRIGHTS_HEADLINE" /></h4>
+            <p className="ng-scope"><Trans id="AS_IMPRINT_COPYRIGHTS_P" /></p>
 
           </div>
         </content-row>
