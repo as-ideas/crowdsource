ef8f66acec54a1744ec5fe9856b38d0cb6e382b4
Madany Mamlouk, Tarek <tarek.madanymamlouk@asideas.de>
2019-09-26 13:16:55 +0200

    restore defaults

--- crowdsource-frontend/src/main/pages/ImprintView/ImprintView.js
+++ crowdsource-frontend/src/main/pages/ImprintView/ImprintView.js
@@ -10,18 +10,55 @@ export default class ImprintView extends React.Component {
         <div className="teaser--slim ng-scope"></div>
         <content-row className="imprint ng-scope">
           <div className="container" ng-transclude="">
-            <h2 className="ng-scope"><Trans id="AS_IMPRINT_HEADLINE" /></h2>
+            <h2 className="ng-scope"><Trans id="AS_IMPRINT_HEADLINE">Impressum</Trans></h2>
 
-            <h4 className="ng-scope"><Trans id="AS_IMPRINT_PROVIDER" /></h4>
-            <address className="ng-scope"><Trans id="AS_IMPRINT_ADDRESS" /></address>
+            <h4 className="ng-scope"><Trans id="AS_IMPRINT_PROVIDER">Anbieter</Trans></h4>
 
-            <h4 className="ng-scope"><Trans id="AS_IMPRINT_CONTACT_HEADLINE" /></h4>
-            <p translate-value-email="<a href='mailto:crowd@asideas.de'>crowd@asideas.de</a>" className="ng-scope"><Trans id="AS_IMPRINT_CONTACT_P" /></p>
+            <address className="ng-scope">
+              <Trans id="AS_IMPRINT_ADDRESS">
+                Axel Springer Ideas Engineering GmbH<br/>
+                Ein Unternehmen der Axel Springer SE<br/>
+                Axel-Springer-Straße 65<br/>
+                10888 Berlin
+              </Trans>
+            </address>
 
-            <h4 className="ng-scope"><Trans id="AS_IMPRINT_RESPONSIBLE_HEADLINE" /></h4>
-            <p className="ng-scope"><Trans id="AS_IMPRINT_RESPONSIBLE_P" /></p>
+            <h4 className="ng-scope"><Trans id="AS_IMPRINT_CONTACT_HEADLINE">Kontakt</Trans></h4>
+            <p translate-value-email="<a href='mailto:crowd@asideas.de'>crowd@asideas.de</a>" className="ng-scope">
+              <Trans id="AS_IMPRINT_CONTACT_P">
+                E-Mail: <a href="mailto:crowd@asideas.de">crowd@asideas.de</a><br/>
+                Telefon: 030 - 2591 78100<br/>
+              </Trans>
+            </p>
+
+            <h4 className="ng-scope">
+              <Trans id="AS_IMPRINT_RESPONSIBLE_HEADLINE">
+                Verantwortlich für den Inhalt nach § 6 Abs.2 MDStV
+              </Trans>
+            </h4>
+
+            <p className="ng-scope">
+              <Trans id="AS_IMPRINT_RESPONSIBLE_P">
+                Michael Alber<br/>
+                COO<br/>
+                Axel-Springer-Straße 65<br/>
+                10888 Berlin<br/>
+                <br/>
+                Amtsgericht/ Handelsregister<br/>
+                Sitz Berlin, Amtsgericht Charlottenburg, HRB 138466 B<br/>
+                USt-IdNr. DE 287499537<br/>
+                Geschäftsführer: Samir Fadlallah, Michael Alber<br/>
+              </Trans>
+            </p>
+
             <br className="ng-scope" />
-            <h4 className="ng-scope"><Trans id="AS_IMPRINT_COPYRIGHTS_HEADLINE" /></h4>
+
+            <h4 className="ng-scope">
+              <Trans id="AS_IMPRINT_COPYRIGHTS_HEADLINE">
+                Bildrechte
+              </Trans>
+            </h4>
+
             <p className="ng-scope"><Trans id="AS_IMPRINT_COPYRIGHTS_P" /></p>
 
           </div>
